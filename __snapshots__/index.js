0547b8c2a69c6e2a44bf64631dad5634bafbb89b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-21 17:44:48 -0400

    add snapshots

---
+++ __snapshots__/index.js
@@ -0,0 +1,46 @@
+exports['runs only tests with "does" in their name from spec.js 1'] = {
+  "main stats": {
+    "suites": 2,
+    "tests": 4,
+    "passes": 3,
+    "pending": 1,
+    "skipped": 0,
+    "failures": 0
+  }
+}
+
+exports['runs only tests with "does" in their name from spec.js 2'] = {
+  "test state": [
+    {
+      "title": [
+        "Example tests",
+        "works"
+      ],
+      "state": "pending"
+    },
+    {
+      "title": [
+        "Example tests",
+        "nested",
+        "does A"
+      ],
+      "state": "passed"
+    },
+    {
+      "title": [
+        "Example tests",
+        "nested",
+        "does B"
+      ],
+      "state": "passed"
+    },
+    {
+      "title": [
+        "Example tests",
+        "nested",
+        "does C"
+      ],
+      "state": "passed"
+    }
+  ]
+}
---
+++ test/index.js
@@ -0,0 +1,38 @@
+const R = require('ramda')
+const la = require('lazy-ass')
+const snapshot = require('snap-shot-it')
+const cypress = require('cypress')
+
+it('runs only tests with "does" in their name from spec.js', () => {
+  return cypress
+    .run({
+      env: {
+        grep: 'does'
+      },
+      config: {
+        video: false,
+        videoUploadOnPasses: false
+      },
+      spec: 'cypress/integration/spec.js'
+    })
+    .then(R.prop('runs'))
+    .then(runs => {
+      la(runs.length === 1, 'expected single run', runs)
+      return runs[0]
+    })
+    .then(run => {
+      run => console.log('%O', run)
+
+      snapshot({
+        'main stats': R.pick(
+          ['suites', 'tests', 'passes', 'pending', 'skipped', 'failures'],
+          run.stats
+        )
+      })
+
+      const testInfo = R.project(['title', 'state'], run.tests)
+      snapshot({
+        'test state': testInfo
+      })
+    })
+})
