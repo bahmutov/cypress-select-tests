5be9fba37c06741d9e83c5606bd7b6b113b32e64
Lê Hoàng Phương <lehoangphuongbg@gmail.com>
2019-04-26 14:17:42 +0900

    Preserve default browserify configuration

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const itify = require('./itify')
 const onFilePreprocessor = (config, pickTests) => {
   const options = {
     browserifyOptions: {
-      extensions: ['.js'],
-      transform: itify(config, pickTests)
+      transform: [
+          ...browserify.defaultOptions.browserifyOptions.transform,
+          itify(config, pickTests)
+      ]
     }
   }
 
